847815c7d01a4c60b6bb5f97e670da9c5b529b8d
gacarrasquilla <gennys.carrasquilla@platypus-agency.com>
2020-07-15 14:01:22 -0500

    Docs

---
+++ docs/precache-manifest.c7e7c1330128de29537c438a824c560a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035893b6e4d598a27020674f68fad832",
+    "url": "/index.html"
+  },
+  {
+    "revision": "922bfd50982dfb183eea",
+    "url": "/static/css/main.a1790d00.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "922bfd50982dfb183eea",
+    "url": "/static/js/main.a56d533a.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
